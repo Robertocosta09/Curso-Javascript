82b5d9023d8f7a94758ec9c1e335f15b4b591e94
Roberto Costa <robertohsantos19@gmail.com>
2023-07-31 21:59:03 +0100

    javascript

---
+++ Aula1.js
@@ -0,0 +1,16 @@
+let numero1 = 10
+let numero2 = 20
+
+let resultadoSoma = numero1 + numero2
+let resultadoSubtracao = numero1 - numero2
+let resultadoProduto = numero1 * numero2
+
+console.log(resultadoSoma)
+console.log(resultadoSubtracao)
+console.log(resultadoProduto)
+
+let nomeInserido = prompt("DigiteNome");
+
+let nome1 = "Roberto"
+let nome2 = "Costa"
+console.log(nome1 + " " + nome2)
