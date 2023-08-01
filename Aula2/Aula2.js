58043f82f51055bc89a85cdff082094869c900ee
Roberto Costa <robertohsantos19@gmail.com>
2023-08-01 22:18:59 +0100

    Desafio2

---
+++ Aula2/Aula2.js
@@ -0,0 +1,12 @@
+let numero1 = 1000
+let nome = "Olá"
+let numero2 = 11
+if ("numero1 > 1000") {
+    alert("Número maior que mil")
+}
+if (nome == "Olá") {
+    alert("Olá")
+}
+if (numero2 >= 10 && numero2 <= 50) {
+    alert("Número maior que 10 e menor que 50")
+}
