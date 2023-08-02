dff4795ee5a984cfe9558b934919e6a12835a0cb
Roberto Costa <robertohsantos19@gmail.com>
2023-08-02 23:39:04 +0100

    desafio2

--- Desafio Aula2/Desafio Aula2.js
+++ Desafio Aula2/Desafio Aula2.js
@@ -1,12 +1,23 @@
-let numero1 = 1000
-let nome = "Olá"
-let numero2 = 11
-if ("numero1 > 1000") {
-    alert("Número maior que mil")
-}
-if (nome == "Olá") {
-    alert("Olá")
-}
-if (numero2 >= 10 && numero2 <= 50) {
-    alert("Número maior que 10 e menor que 50")
+
+let valor1 = parseInt(prompt("Digite o primeiro valor:"));
+let valor2 = parseInt(prompt("Digite o segundo valor:"));
+
+// Verifica se os valores são iguais
+if (valor1 === valor2) {
+    console.log("Os valores são iguais.");
+    alert("Os valores são iguais.");
+} else {
+    // Verifica qual valor é maior
+    if (valor1 > valor2) {
+        console.log("valor 1 é maior que valor 2");
+        alert("Valor 1 é maior que valor 2");
+    } else {
+        console.log("Valor 2 é maior que valor 1");
+        alert("Valor 2 é maior que valor 1");
+    }
+
+    // Calcula e exibe a soma dos valores
+    const sum = valor1 + valor2;
+    console.log(`A soma dos valores é: ${sum}.`);
+    alert(`A soma dos valores é: ${sum}.`);
 }
