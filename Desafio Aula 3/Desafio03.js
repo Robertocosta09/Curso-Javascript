6019d3b0ceba0f7bb1501951c8356343216898b1
Roberto Costa <robertohsantos19@gmail.com>
2023-08-06 22:58:53 +0100

    desafio03

--- Desafio Aula 3/Desafio03.js
+++ Desafio Aula 3/Desafio03.js
@@ -27,4 +27,26 @@ if (isNaN(numero)) {
     }
 }
 
+//for
+for (let i = 1; i <= 5; i++) {
 
+    let resultado = i * 2;
+
+    console.log('Resultado da iteração ' + i + ': ' + resultado);
+}
+
+//while
+let contador = 1;
+const limite = 5;
+
+while (contador <= limite) {
+
+    if (contador % 2 !== 0) {
+
+        console.log('O contador ' + contador + ' é ímpar.');
+    } else {
+        console.log('O contador ' + contador + ' é par.');
+    }
+
+    contador++;
+}
